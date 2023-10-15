05032208c2b0f0bc8b60aaf82afa39158b1fdf39
Vlad Croicu (Zoli) <101589800+Zolianul@users.noreply.github.com>
2023-10-15 19:22:11 +0300

    DistanceCounter
    Added a counter for the distance walked in game.

---
+++ Assets/Scripts/Collectables/LevelDistance.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelDistance : MonoBehaviour
+{
+    
+    public GameObject distDisplay;
+    public int distRun;
+    public bool addingDist = false;
+
+    public float distDelay = 0.35f;
+    
+    void Update()
+    {
+        if(addingDist == false)
+        {
+            addingDist = true;
+            StartCoroutine(AddingDist());
+        }    
+    }
+
+    IEnumerator AddingDist()
+    {
+        distRun += 1;
+        distDisplay.GetComponent<Text>().text = ""+distRun;
+        yield return new WaitForSeconds(distDelay);
+        addingDist = false;
+    }
+}
