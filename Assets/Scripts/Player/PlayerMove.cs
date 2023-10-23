32a279ceceda2a00002fa332350357c849aceab7
Vlad Croicu (Zoli) <101589800+Zolianul@users.noreply.github.com>
2023-10-23 19:56:25 +0300

    jump function
    added the ability to jump

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -8,6 +8,10 @@ public class PlayerMove : MonoBehaviour
     public float moveSpeed = 5;
     public float leftRightSpeed = 4;
     static public bool canMove= false;
+    public bool isJumping = false;
+    public bool comingDown = false;
+    public GameObject playerObject;
+
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
@@ -21,18 +25,48 @@ public class PlayerMove : MonoBehaviour
                 }
 
             }
-            if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
-                return;
+                if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+                {
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+                }
             }
-            if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
             {
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+                if( isJumping == false )
+                {
+                    isJumping = true;
+                    playerObject.GetComponent <Animator>().Play("Jump");
+                    StartCoroutine(JumpSequence());
+                }
+
             }
         }
 
-
+        if(isJumping == true)
+        {
+            if(comingDown == false)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * 3, Space.World);// aici 3 sau -3 dicteaza cat de sus se sare - asta poate fi o variabila si putem face super jump power up
+            }
+            else
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * -3, Space.World);
+            }
+        }
     }
 
+        IEnumerator JumpSequence()
+        {
+            yield return new WaitForSeconds(0.45f);
+            comingDown=true;
+            yield return new WaitForSeconds(0.45f);
+            isJumping=false;
+            comingDown=false;
+            playerObject.GetComponent <Animator>().Play("Standard Run");
 
+        }
 }
