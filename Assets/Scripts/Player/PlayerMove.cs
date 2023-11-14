cb33803558a5af700dd04dedbec512da77a13dc3
Vlad Croicu (Zoli) <101589800+Zolianul@users.noreply.github.com>
2023-11-14 17:42:43 +0200

    JumpUpdates

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -12,15 +12,19 @@ public class PlayerMove : MonoBehaviour
     public bool comingDown = false;
     public GameObject playerObject;
     public float maxMoveSpeed = 20.0f;
+    public float initialYPosition;
     
-
+    void Start()
+    {
+        initialYPosition=transform.position.y;
+    }
 
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
         if(canMove == true)
         {
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow ) || SwipeManager.swipeLeft)
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow )/*|| SwipeManager.swipeLeft*/)
             {
                 if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
                 {
@@ -28,7 +32,7 @@ public class PlayerMove : MonoBehaviour
                 }
 
             }
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || SwipeManager.swipeRight)
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) /*|| SwipeManager.swipeRight*/)
             {
                 if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
                 {
@@ -37,7 +41,7 @@ public class PlayerMove : MonoBehaviour
             }
             
 
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space) || SwipeManager.swipeUp)
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space) /*|| SwipeManager.swipeUp*/)
             {
                 if( isJumping == false )
                 {
@@ -60,7 +64,7 @@ public class PlayerMove : MonoBehaviour
         {
             if(comingDown == false)
             {
-                transform.Translate(Vector3.up * Time.deltaTime * 4, Space.World);// aici 3 sau -3 dicteaza cat de sus se sare - asta poate fi o variabila si putem face super jump power up
+                transform.Translate(Vector3.up * Time.deltaTime * 4, Space.World);
             }
             if (comingDown == true)
             {
@@ -69,14 +73,28 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-        IEnumerator JumpSequence()
-        {
-            yield return new WaitForSeconds(0.5f);
-            comingDown=true;
-            yield return new WaitForSeconds(0.5f);
-            isJumping=false;
-            comingDown=false;
-            playerObject.GetComponent <Animator>().Play("Standard Run");
-           
+       IEnumerator JumpSequence()
+{
+    float jumpTime = 1.1f; // Adjust this value based on your needs
+    float jumpHeight = 3.0f; // Adjust this value based on your needs
+
+    float elapsedTime = 0f;
+    while (elapsedTime < jumpTime)
+    {
+        float jumpProgress = elapsedTime / jumpTime;
+        float jumpHeightOffset = Mathf.Sin(jumpProgress * Mathf.PI) * jumpHeight;
+
+        transform.position = new Vector3(
+            transform.position.x,
+            initialYPosition + jumpHeightOffset,
+            transform.position.z
+        );
+
+        elapsedTime += Time.deltaTime;
+        yield return null;
     }
+
+    isJumping = false;
+    playerObject.GetComponent<Animator>().Play("Standard Run");
+}
 }
